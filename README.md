fd046a569057bbf90b189a33b5784e3d9517d4b9
Grace Nieboer <grace.nieboer@outlook.com>
2024-07-23 12:55:22 -0400

    algo solved - and prelim tests done

---
+++ README.md
@@ -0,0 +1,6 @@
+# Reverse a String
+
+Write a program that takes a string in JavaScript, and reverses it.
+
+For example, "zebra" would reverse to "arbez"
+
---
+++ app.js
@@ -0,0 +1,13 @@
+function reverse_string (word) {
+    // loop thru each letter
+    let wordArray = []
+    for (let i = word.length-1; i >= 0 ; i-=1) {
+        wordArray.push(word[i])
+        
+    }
+    // console.log(wordArray)
+    return wordArray.join("")
+}
+
+console.log("Reverse hello: ", reverse_string('hello'))
+console.log("should be: olleh")
